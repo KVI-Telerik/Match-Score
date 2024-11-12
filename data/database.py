199373f0671181735f149f26a991a014956911a7
gviktor13 <vikgeo13@gmail.com>
2024-11-12 18:22:00 +0200

    some empty files

--- data/database.py
+++ data/database.py
@@ -1,14 +1,14 @@
 import psycopg2
 from psycopg2.extensions import connection as Connection
 
-# Change password to make requests
+
 def _get_connection() -> Connection:
     return psycopg2.connect(
-        user='root',
-        password='postgres123', 
-        host='localhost',
-        port=5432,  # Default PostgreSQL port
-        database='forumdb'
+        user='match-score_owner',
+        password='3qem5dgbONsA',
+        host='ep-broad-morning-a2defk2e.eu-central-1.aws.neon.tech',
+        port=5432,
+        database='match-score'
     )
 
 def read_query(sql: str, sql_params=()):
